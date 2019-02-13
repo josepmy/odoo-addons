abf0245a410c611dd9765776a3459fb81a0711f6
jffernandez <jffernandez@fenix-es.com>
2019-02-13 17:53:47 +0100

    Add project_kanban_documents module

---
+++ project_kanban_documents/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Fenix Engineering Solutions
+# @author Jose F. Fernandez
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': "Project Kanban Documents",
+    'version': "11.0.1.0.1",
+    'category': "Project",
+    'sequence': 10,
+    'license': 'AGPL-3',
+    'author': "Fenix Engineering Solutions",
+    'website': "http://www.fenix-es.com",
+    'depends': ['project', ],
+    'data': [
+        'views/project_inherit.xml',
+    ],
+    'demo': [
+    ],
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    'summary': "Show documents action on project kanban view",
+    'description': """
+Project Kanban Documents
+========================
+
+Show documents action on project kanban view.
+""",
+}
