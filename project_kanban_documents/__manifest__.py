1b9d6f53aeaabea449254eeb4ac708b2dd78d05c
jffernandez <jffernandez@fenix-es.com>
2019-02-14 16:06:59 +0100

    Add missing file

--- project_kanban_documents/__manifest__.py
+++ project_kanban_documents/__manifest__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': "Project Kanban Documents",
-    'version': "11.0.1.0.1",
+    'version': "11.0.1.0.2",
     'category': "Project",
     'sequence': 10,
     'license': 'AGPL-3',
