3bdc1f801f646eab92d31f235e9e281edbbc6a85
jffernandez <jffernandez@fenix-es.com>
2019-02-14 16:07:36 +0100

    Add project_task_documents module

---
+++ project_task_documents/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Fenix Engineering Solutions
+# @author Jose F. Fernandez
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': "Project Task Documents",
+    'version': "11.0.1.0.1",
+    'category': "Project",
+    'sequence': 10,
+    'license': 'AGPL-3',
+    'author': "Fenix Engineering Solutions",
+    'website': "http://www.fenix-es.com",
+    'depends': ['project', ],
+    'data': [
+        'views/project_inherit.xml',
+    ],
+    'demo': [
+    ],
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    'summary': "Add documents to project task",
+    'description': """
+Project Task Documents
+======================
+
+Add documents button on project task and on kanban view of task.
+""",
+}
--- project_task_documents/models/__init__.py
+++ project_task_documents/models/__init__.py
@@ -3,4 +3,4 @@
 # @author Jose F. Fernandez
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import models
+from . import project
