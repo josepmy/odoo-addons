46be3244eab59edf12b433c4144ac2f86e88968d
jffernandez <jffernandez@fenix-es.com>
2019-04-22 23:48:25 +0200

    Add module project_task_checklist

---
+++ project_task_checklist/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Fenix Engineering Solutions
+# @author Jose F. Fernandez
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+{
+    'name': "Project Task Checklist",
+    'version': "11.0.1.1.8",
+    'category': "Project",
+    'sequence': 10,
+    'summary': "Add checklist to project task",
+    'license': 'LGPL-3',
+    'author': "Fenix Engineering Solutions",
+    'website': "http://www.fenix-es.com",
+    'images': [
+        'static/description/cover.png'
+    ],
+    'depends': ['project', ],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/security.xml',
+        'views/project_inherit.xml',
+    ],
+    'demo': [
+    ],
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    'description': """
+Project Task Checklist
+======================
+
+Add checklist to project task.
+""",
+}
---
+++ project_task_checklist/models/project.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Fenix Engineering Solutions
+# @author Jose F. Fernandez
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from odoo import api, fields, models, _
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class ProjectTask(models.Model):
+    _inherit = 'project.task'
+
+    checklist_name = fields.Char(string='Checklist name')
+    progress_from_checklist = fields.Boolean(string='Checklist progress')
+    checklist_item_ids = fields.One2many('project_task_checklist.item', 'task_id', string='Checklist elements')
+
